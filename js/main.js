87c22763b2066acad28a2a8d0ea65f7491cfcc78
Annie Huang <annie.hn@gmail.com>
2023-12-01 17:26:47 +1100

    Get hold of the .faq-group-header user clicks

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const faqContainer = document.querySelector('.faq-content');
 
-  faqContainer.addEventListener('click', () => {
-    alert(1);
+  faqContainer.addEventListener('click', (e) => {
+    // alert(1);
+    // console.log(e.target);
+    const groupHeader = e.target.closest('.faq-group-header');
+
+    // Case when user clicks onto the .faq-group-body, groupHeader will return null;
+    if (!groupHeader) return;
+
+    console.log(groupHeader);
   });
 });
